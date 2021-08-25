163f3f434578e581a3e213002ee03161b5fa8481
roboto84 <7658704+roboto84@users.noreply.github.com>
2021-08-25 08:56:02 -0400

    refactor(bot): remove print statements and update deps

--- lexicon_bot/lexicon_bot.py
+++ lexicon_bot/lexicon_bot.py
@@ -2,7 +2,7 @@
 import os
 import ast
 import logging.config
-from typing import Any, NoReturn
+from typing import Any
 from dotenv import load_dotenv
 from lexicon.library.lexicon import Lexicon
 from wh00t_core.library.client_network import ClientNetwork
@@ -11,32 +11,26 @@ from wh00t_core.library.client_network import ClientNetwork
 class LexiconBot:
     def __init__(self, logging_object: Any, socket_host: str, socket_port: int, webster_key: str, oxford_app_id: str,
                  oxford_key: str):
-        self.logger: Any = logging_object.getLogger(type(self).__name__)
+        self.logger: logging.Logger = logging_object.getLogger(type(self).__name__)
         self.logger.setLevel(logging.INFO)
-        self.socket_host: str = socket_host
-        self.socket_port: int = socket_port
-        self.receive_thread: Any = None
-        self.lexicon = Lexicon(webster_key, oxford_app_id, oxford_key)
-        self.socket_network: ClientNetwork = ClientNetwork(self.socket_host, self.socket_port,
+        self.lexicon: Lexicon = Lexicon(webster_key, oxford_app_id, oxford_key)
+        self.socket_network: ClientNetwork = ClientNetwork(socket_host, socket_port,
                                                            'lexicon_bot', 'app', logging)
 
-    def run_bot(self) -> NoReturn:
+    def run_bot(self) -> None:
         self.socket_network.sock_it()
         self.socket_network.receive(self.__receive_message_callback)
 
-    def __receive_message_callback(self, package) -> NoReturn:
-        print(package)
+    def __receive_message_callback(self, package: dict) -> bool:
         if package['id'] not in ['wh00t_server', 'lexicon_bot'] and 'category' in package \
                 and package['category'] == 'get_word_definition' and 'message' in package:
             message: dict = ast.literal_eval(package['message'])
-            print(message)
-            search_word = message['search_word']
-            print(search_word)
+            search_word: str = message['search_word']
             self.__send_dictionary_data(search_word)
+        return True
 
-    def __send_dictionary_data(self, search_word) -> NoReturn:
-        dictionary_payload = self.get_data(search_word)
-        print(dictionary_payload)
+    def __send_dictionary_data(self, search_word) -> None:
+        dictionary_payload: dict = self.get_data(search_word)
         self.socket_network.send_message('word_definition', str(dictionary_payload['oxford']))
 
     def get_data(self, search_word: str) -> dict:
@@ -45,7 +39,7 @@ class LexiconBot:
 
 if __name__ == '__main__':
     logging.config.fileConfig(fname=os.path.abspath('lexicon_bot/bin/logging.conf'), disable_existing_loggers=False)
-    logger: Any = logging.getLogger(__name__)
+    logger: logging.Logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
     try:
@@ -57,8 +51,8 @@ if __name__ == '__main__':
         OXFORD_APP_KEY: str = os.getenv('OXFORD_APP_KEY')
 
         print(f'\nLexicon data sender will now continuously run')
-        lexicon = LexiconBot(logging, HOST_SERVER_ADDRESS, SOCKET_SERVER_PORT, MERRIAM_WEBSTER_API_KEY,
-                             OXFORD_APP_ID, OXFORD_APP_KEY)
+        lexicon: LexiconBot = LexiconBot(logging, HOST_SERVER_ADDRESS, SOCKET_SERVER_PORT, MERRIAM_WEBSTER_API_KEY,
+                                         OXFORD_APP_ID, OXFORD_APP_KEY)
         lexicon.run_bot()
     except TypeError:
         logger.error('Received TypeError: Check that the .env project file is configured correctly')
