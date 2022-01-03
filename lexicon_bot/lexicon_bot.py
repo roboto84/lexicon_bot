977ec0abcc5812c10cb76ede20c23c6f365a5b66
roboto84 <7658704+roboto84@users.noreply.github.com>
2022-01-03 08:19:25 -0500

    refactor(chat): remove word lookup chat response

--- lexicon_bot/lexicon_bot.py
+++ lexicon_bot/lexicon_bot.py
@@ -1,12 +1,9 @@
 # Air data sender
 import os
-import ast
 import logging.config
 from typing import Any
 from dotenv import load_dotenv
 from lexicon.library.lexicon import Lexicon
-from bin.lexicon_bot_helper import LexiconBotHelper
-from lexicon_core.library.db.lexicon_db import LexiconDb
 from wh00t_core.library.client_network import ClientNetwork
 
 
@@ -16,8 +13,7 @@ class LexiconBot:
         self._logger: logging.Logger = logging_object.getLogger(type(self).__name__)
         self._logger.setLevel(logging.INFO)
         self._chat_key: str = '/lexi '
-        self._lexicon: Lexicon = Lexicon(webster_key, oxford_app_id, oxford_key)
-        self._lexicon_db = LexiconDb(logging_object, sql_lite_db_path)
+        self._lexicon: Lexicon = Lexicon(webster_key, oxford_app_id, oxford_key, sql_lite_db_path, logging_object)
         self._socket_network: ClientNetwork = ClientNetwork(socket_host, socket_port, 'lexicon_bot', 'app', logging)
 
     def run_bot(self) -> None:
@@ -26,41 +22,20 @@ class LexiconBot:
             self._socket_network.receive(self._receive_message_callback)
         except KeyboardInterrupt:
             self._logger.info('Received a KeyboardInterrupt... closing bot')
-            os._exit(1)
+            exit()
 
     def _receive_message_callback(self, package: dict) -> bool:
         if ('id' in package) and (package['id'] not in ['wh00t_server', 'lexicon_bot']) and ('message' in package):
-            if 'category' in package and package['category'] == 'get_word_definition':
-                message_package: dict = ast.literal_eval(package['message'])
-                search_word: str = message_package['search_word']
-                self._send_api_data(search_word)
-            elif 'category' in package and package['category'] == 'chat_message' and \
+            if 'category' in package and package['category'] == 'chat_message' and \
                     isinstance(package['message'], str) and self._chat_key in package['message']:
                 search_word: str = package['message'].replace(self._chat_key, '')
                 self._send_chat_data(search_word)
         return True
 
-    def _send_api_data(self, search_word: str) -> None:
-        data = self._lexicon.get_dictionary_def(search_word)
-        self._socket_network.send_message('word_definition', str(data))
-        if LexiconBotHelper.acceptable_def_result_comparator(data):
-            self._lexicon_db.insert_word(LexiconBotHelper.parse_dictionary_data(data))
-
     def _send_chat_data(self, search_word: str):
         self._socket_network.send_message('chat_message', f'Ok, defining "{search_word}" 🤔')
-        data = self._lexicon.get_dictionary_def(search_word)
-
-        if LexiconBotHelper.acceptable_def_result_comparator(data):
-            data = LexiconBotHelper.parse_dictionary_data(data)
-            self._lexicon_db.insert_word(data)
-            word_def_chat_message = LexiconBotHelper.chat_message_builder(data)
-        elif 'spelling_suggestions' in data and len(data['spelling_suggestions']) > 0:
-            word_def_chat_message = 'Looks like the word is misspelled. Perhaps try ...\n\n'
-            spell_suggestions = ''.join(f'{spell_suggestion}, ' for spell_suggestion in data['spelling_suggestions'])
-            word_def_chat_message = f'{word_def_chat_message}{spell_suggestions}'.rstrip(',')
-        else:
-            word_def_chat_message = 'Sorry, no information on that word was found.'
-        self._socket_network.send_message('chat_message', word_def_chat_message)
+        dictionary_summary: str = self._lexicon.definition_summary(self._lexicon.get_definition(search_word))
+        self._socket_network.send_message('chat_message', dictionary_summary)
 
 
 if __name__ == '__main__':
@@ -78,8 +53,15 @@ if __name__ == '__main__':
         SQL_LITE_DB: str = os.getenv('SQL_LITE_DB')
 
         print(f'\nLexicon data sender will now continuously run')
-        lexicon: LexiconBot = LexiconBot(logging, HOST_SERVER_ADDRESS, SOCKET_SERVER_PORT, MERRIAM_WEBSTER_API_KEY,
-                                         OXFORD_APP_ID, OXFORD_APP_KEY, SQL_LITE_DB)
+        lexicon: LexiconBot = LexiconBot(
+            logging,
+            HOST_SERVER_ADDRESS,
+            SOCKET_SERVER_PORT,
+            MERRIAM_WEBSTER_API_KEY,
+            OXFORD_APP_ID,
+            OXFORD_APP_KEY,
+            SQL_LITE_DB
+        )
         lexicon.run_bot()
     except TypeError:
         logger.error('Received TypeError: Check that the .env project file is configured correctly')
